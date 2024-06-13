1d9eff1157e72a8a5961905348b51e3bda08a382
Stephen Molloy <sdmolloy@gmail.com>
2024-06-13 15:11:38 +0200

    Dot can work as a prefix op

--- json_experiment.pl
+++ json_experiment.pl
@@ -10,6 +10,12 @@ jsonfile_rfdumpinfo(Filename, RfJson) :-
   fname_json(Filename, JT),
   RfJson = JT.rfdump.
 
+
+
 main :-
-  jsonfile_rfdumpinfo("R3_B080603_CAB02_VAC_PLC01.json", Rfdump), dict_keys(Rfdump, Ks),
-  portray_clause(Ks).
+  Fname = "R3_B080603_CAB02_VAC_PLC01.json",
+  jsonfile_rfdumpinfo(Fname, Rfdump),
+  dict_keys(Rfdump, Ks),
+  Ks = [K|_],
+  X = .(Rfdump, K),
+  portray_clause(X).
