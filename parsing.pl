03f8c0ae052e67a39492aa7239abc5eb297aa552
Stephen Molloy <sdmolloy@gmail.com>
2024-06-15 19:38:07 +0200

    Reorg to remove unnecessary code

--- parsing.pl
+++ parsing.pl
@@ -1,5 +1,7 @@
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
+:- use_module(library(tabling)).
+:- table statement/3.
 
 ws --> [W], { char_type(W, whitespace) }, ws.
 ws --> [].
@@ -8,13 +10,14 @@ ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
 
-statement([Id1, Op, Id2]) --> ident(Id1), binop(Op), ident(Id2).
-statement([Id1, Op, Id2]) --> ident(Id1), equalop(Op), ident(Id2).
-statement([Op, Id2]) --> unop(Op), ident(Id2).
+statement(Id) --> ident(Id).
+statement([Id1, Op, Id2]) --> statement(Id1), binop(Op), statement(Id2).
+statement([Op, Id2]) --> unop(Op), statement(Id2).
 
-binop(and) --> seq(Ws), { Ws="AND" }.
-binop(or) --> seq(Ws), { Ws="OR" }.
-unop(not) --> seq(Ws), { Ws="NOT" }.
+statement(S) --> ws, seq("("), statement(S), seq(")"), ws.
 
-equalop(testequals) --> seq(Ws), { Ws="="}.
+binop(and)    --> "AND".
+binop(or)     --> "OR".
+binop(equals) --> "=".
+unop(not)     --> "NOT".
 
