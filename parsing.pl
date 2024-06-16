317903998825d1ea77220c631e3a5d79d59469bb
Stephen Molloy <sdmolloy@gmail.com>
2024-06-16 21:47:44 +0200

    Removed tabling as well as the left recursion

--- parsing.pl
+++ parsing.pl
@@ -1,20 +1,19 @@
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
-:- use_module(library(tabling)).
-:- table statement/3.
 
 ws --> [W], { char_type(W, whitespace) }, ws.
 ws --> [].
 
+statement(T) --> term(T).
+statement([Id1, Op, Id2]) --> term(Id1), binop(Op), statement(Id2).
+
+term(Id)        --> ident(Id).
+term([Unop,Id]) --> unop(Unop), statement(Id).
+term(S)         --> ws, "(", statement(S), ")", ws.
+
 ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
-
-statement(Id) --> ident(Id).
-statement([Unop, Id]) --> unop(Unop), statement(Id).
-statement([Id1, Op, Id2]) --> statement(Id1), binop(Op), statement(Id2).
-
-statement(S) --> ws, seq("("), statement(S), seq(")"), ws.
 
 binop(and)    --> ws, "AND", ws.
 binop(or)     --> ws, "OR", ws.
