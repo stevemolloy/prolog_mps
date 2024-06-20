fbce73fbc2194164bf697a8aac1fd767baaa8e0b
Stephen Molloy <sdmolloy@gmail.com>
2024-06-20 13:31:55 +0200

    Some testing code

--- test.pl
+++ test.pl
@@ -1,3 +1,9 @@
-# :- use_module(library(dicts)).
+% :- use_module(library(dicts)).
 
 json({ "name": "John Doe", "age": 30, "city": "New York" }).
+% json({name: "John Doe", age: 30, city: "New York"}).
+
+extract_name({"name": Name, "age": _, "city": _}, Name).
+extract_age({"name": _, "age": Age, "city": _}, Age).
+extract_city({"name": _, "age": _, "city": City}, City).
+
