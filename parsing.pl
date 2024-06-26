1a75a403b05ebee51dfdbd95a489e03b90cd25b0
Stephen Molloy <sdmolloy@gmail.com>
2024-06-26 18:24:45 +0200

    Parser deals with something much closer to a proper AST

--- parsing.pl
+++ parsing.pl
@@ -7,18 +7,15 @@ ws --> [W], { char_type(W, whitespace) }, !, ws.
 ws --> [].
 
 statement(T) --> term(T).
-statement([Id1, Op, Id2]) --> term(Id1), binop(Op), statement(Id2).
+statement(and(Id1, Id2))    --> term(Id1), ws, "AND", ws, statement(Id2).
+statement(or(Id1, Id2))     --> term(Id1), ws, "OR", ws, statement(Id2).
+statement(equals(Id1, Id2)) --> term(Id1), ws, "=", ws, statement(Id2).
 
-term(Id)        --> ident(Id).
-term([Unop,Id]) --> unop(Unop), term(Id).
-term(S)         --> ws, "(", statement(S), ")", ws.
+term(Id)      --> ident(Id).
+term(not(Id)) --> ws, "NOT", ws, term(Id).
+term(S)       --> ws, "(", statement(S), ")", ws.
 
 ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
-
-binop(and)    --> ws, "AND", ws.
-binop(or)     --> ws, "OR", ws.
-binop(equals) --> ws, "=", ws.
-unop(not)     --> ws, "NOT", ws.
 
