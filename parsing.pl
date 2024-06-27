51ca79f344efd2e3e38910d7afeb9e0bb59a13ab
Stephen Molloy <sdmolloy@gmail.com>
2024-06-27 09:55:05 +0200

    Extend parsing module to include ast_clpbexpr predicate

--- parsing.pl
+++ parsing.pl
@@ -1,7 +1,8 @@
-:- module(parsing, [statement//1]).
+:- module(parsing, [statement//1, ast_clpbexpr/2]).
 
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
+:- use_module(library(lists)).
 
 ws --> [W], { char_type(W, whitespace) }, !, ws.
 ws --> [].
@@ -18,4 +19,17 @@ term(S)       --> ws, "(", statement(S), ")", ws.
 ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
+
+ast_clpbexpr(id(Id), Id).
+ast_clpbexpr(not(T1), Expr) :-
+  ast_clpbexpr(T1, Expr1),
+  append(["( ~ ", Expr1, " )"], Expr).
+ast_clpbexpr(or(T1, T2), Expr) :-
+  ast_clpbexpr(T1, Expr1),
+  ast_clpbexpr(T2, Expr2),
+  append(["(", Expr1, " + ", Expr2, ")"], Expr).
+ast_clpbexpr(and(T1, T2), Expr) :-
+  ast_clpbexpr(T1, Expr1),
+  ast_clpbexpr(T2, Expr2),
+  append(["(", Expr1, " * ", Expr2, ")"], Expr).
 
