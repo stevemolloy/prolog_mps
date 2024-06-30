095d06c8b1de8e12e7a7ae0d2c2a61821740ee04
Stephen Molloy <sdmolloy@gmail.com>
2024-06-30 23:30:44 +0200

    Thread an assoc list through the parsing to get data for later

--- parsing.pl
+++ parsing.pl
@@ -1,4 +1,4 @@
-:- module(parsing, [statement//1, ast_clpbchars//1, clpbchars_satchars//1]).
+:- module(parsing, [statement//1, ast_clpbchars//3, clpbchars_satchars//1, string_to_write//3]).
 
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
@@ -23,18 +23,21 @@ ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
 
-ast_clpbchars(id(Id0)) --> { tfilter(dif(.), Id0, Id) }, Id.
-ast_clpbchars(not(T1)) --> "( ~ ", ast_clpbchars(T1), " )".
-ast_clpbchars(or(T1,T2)) -->
-        "(", ast_clpbchars(T1), " + ", ast_clpbchars(T2), ")".
-ast_clpbchars(and(T1,T2)) -->
-        "(", ast_clpbchars(T1), " * ", ast_clpbchars(T2), ")".
+ast_clpbchars(id(Id0), A0, A) --> { tfilter(dif(.), Id0, Id), put_assoc(Id0, A0, Id, A) }, Id.
+ast_clpbchars(not(T1), A0, A) --> "( ~ ", ast_clpbchars(T1, A0, A), " )".
+ast_clpbchars(or(T1,T2), A0, A) -->
+        "(", ast_clpbchars(T1, A0, A1), " + ", ast_clpbchars(T2, A1, A), ")".
+ast_clpbchars(and(T1,T2), A0, A) -->
+        "(", ast_clpbchars(T1, A0, A1), " * ", ast_clpbchars(T2, A1, A), ")".
 
 clpbchars_satchars(C) --> "sat( ~ (", C, ") )".
 
-string_to_write(Name, Es) --> 
+string_to_write(Name, Es, Assoc) --> 
   ":- use_module(library(clpb)).\n\n",
-  Name, " :-", satitem(Es).
+  "asserta(clpb:clpb_residuals(bdd)).\n\n",
+  Name, " :-\n",
+  "  Vs = (", Vals, {assoc_to_values(Assoc, Vals)}, ")", 
+  satitem(Es).
 
 satitem([E]) --> "\n  ", E, ".\n".
 satitem([E | Es]) --> "\n  ", E, ",", satitem(Es).
