97fd81dd2076e7a74fbdfbcceedb490fd451df28
Stephen Molloy <sdmolloy@gmail.com>
2024-07-09 00:01:38 +0200

    A rewrite of the parsing module to allow creation of lists of goals

--- parsing.pl
+++ parsing.pl
@@ -1,4 +1,4 @@
-:- module(parsing, [statement//1, ast_clpbchars//3, clpbchars_satchars//1, string_to_write//3]).
+:- module(parsing, [ast//1, ast_goal/2, asts_goals//1]).
 
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
@@ -10,44 +10,36 @@
 ws --> [W], { char_type(W, whitespace) }, !, ws.
 ws --> [].
 
-statement(T) --> term(T).
-statement(and(Id1, Id2))    --> term(Id1), ws, "AND", ws, statement(Id2).
-statement(or(Id1, Id2))     --> term(Id1), ws, "OR", ws, statement(Id2).
-statement(equals(Id1, Id2)) --> term(Id1), ws, "=", ws, statement(Id2).
+ast(T) --> term(T).
+ast(and(Id1, Id2))    --> term(Id1), ws, "AND", ws, ast(Id2).
+ast(or(Id1, Id2))     --> term(Id1), ws, "OR", ws, ast(Id2).
+ast(equals(Id1, Id2)) --> term(Id1), ws, "=", ws, ast(Id2).
 
 term(id(Id))  --> ident(Id).
 term(not(Id)) --> ws, "NOT", ws, term(Id).
-term(S)       --> ws, "(", statement(S), ")", ws.
+term(S)       --> ws, "(", ast(S), ")", ws.
 
 ident([L|Ls])  --> ws, [L], { char_type(L, alpha)}, symbol(Ls), ws.
 symbol([L|Ls]) --> [L], { char_type(L, ascii_graphic) }, symbol(Ls).
 symbol([])     --> [].
 
-ast_clpbchars(id(Id0), A0, A) --> { tfilter(dif(.), Id0, Id), put_assoc(Id0, A0, Id, A) }, Id.
-ast_clpbchars(not(T1), A0, A) --> "( ~ ", ast_clpbchars(T1, A0, A), " )".
-ast_clpbchars(or(T1,T2), A0, A) -->
-        "(", ast_clpbchars(T1, A0, A1), " + ", ast_clpbchars(T2, A1, A), ")".
-ast_clpbchars(and(T1,T2), A0, A) -->
-        "(", ast_clpbchars(T1, A0, A1), " * ", ast_clpbchars(T2, A1, A), ")".
+ast_term(id(Id), Id).
+ast_term(or(Tree1, Tree2), T) :-
+  T = T1 + T2,
+  ast_term(Tree1, T1),
+  ast_term(Tree2, T2).
+ast_term(and(Tree1, Tree2), T) :-
+  T = T1 * T2,
+  ast_term(Tree1, T1),
+  ast_term(Tree2, T2).
+ast_term(not(Tree), T) :-
+  T = ~T0,
+  ast_term(Tree, T0).
 
-clpbchars_satchars(C) --> "sat( ~ (", C, ") )".
+ast_goal(Ast, Goal) :-
+  ast_term(Ast, Term),
+  Goal = sat(Term).
 
-satlist_predicate(Name, Es, Vals) --> 
-  ":- use_module(library(clpb)).\n",
-  ":- use_module(library(debug)).\n\n",
-  "clpb:clpb_residuals(bdd).\n\n",
-  Name, "(Vs) :-\n",
-  "  Vs = [", vallist(Vals), "],", 
-  "\n  sat( B_R3_RING_CURRENT_LIBHOT_S ),",
-  "\n  sat( B_R3_RING_CURRENT_HOT_S ),",
-  satitems(Es), ",\n",
-  "  labeling(Vs).\n".
-
-vallist([V|Vs]) --> vallist_(Vs, V).
-vallist_([], V) --> V.
-vallist_([V|Vs], V0) --> V0, ", ", vallist_(Vs, V).
-
-satitems([E|Es]) --> satitems_(Es, E).
-satitems_([], E) --> "\n  ", E.
-satitems_([E|Es], E0) --> "\n  ", E0, ",", satitems_(Es, E).
+asts_goals([]) --> [].
+asts_goals([A | As]) --> {ast_goal(A, G)}, [G], asts_goals(As).
 
